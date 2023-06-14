417961e155e2c983ef63b0ddb23f89026a3d871f
AntekR <solanorodriguez.atsr@gmail.com>
2023-06-13 21:17:06 -0500

    actualizados

--- main.js
+++ main.js
@@ -8,32 +8,80 @@ u  -> ufat
 
 */
 
-const textoArea = document.querySelector('.text-area');
-const mensaje = document.querySelector('.mensaje');
-const copia = document.querySelector('.copiar');
-copia.style.display = 'none';
+const textoArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+const copia = document.querySelector(".copiar");
+copia.style.display = "none";
 
-function validar_texto(textoArea);
-    
+function validar() {
+  let text = document.querySelector(".text-area").value;
+  let validador = text.match(/[^a-z\s]/g);
+  console.log(validador);
+  if (!validador == false) {
+    alert("Solo son permitidas letras minúsculas y sin acentos");
+    location.reload();
+    return true;
+  }
+}
 
-function encriptar(textEncriptado){
-    let clave = [['a','enter'],['e','imes'],['i','ai'],['o','ober'],['u','ufat']]
-    let letra = 'aeiou'
-    textEncriptado = textEncriptado.toLowerCase()
-    var textDesencriptado = ''
-    for(let i=0; i < textEncriptado.length; i++){
-        
-        for(let j=0; j< clave.length; j++){
-
-            if (textEncriptado[i]==clave[j][0]) {
-                textDesencriptado+=clave[j][1]
-            }else if(textDesencriptado.includes(textEncriptado[i]) && textEncriptado[i]!=' '){
-                continue
-            }else if(!(letra.includes(textEncriptado[i]))) {
-                textDesencriptado+=textEncriptado[i]
-            }
-        }
-
+function encriptar(textEncriptado) {
+  let clave = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
+  texto = textEncriptado.toLowerCase();
+  for (let i = 0; i < clave.length; i++) {
+    if (texto.includes(clave[i][0])) {
+      texto = texto.replaceAll(clave[i][0], clave[i][1]);
     }
-    return textDesencriptado
+  }
+  return texto;
+}
+
+function desencriptar(textEncriptado) {
+  let clave = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+  ];
+  texto = textEncriptado.toLowerCase();
+  for (let i = 0; i < clave.length; i++) {
+    if (texto.includes(clave[i][1])) {
+      texto = texto.replaceAll(clave[i][1], clave[i][0]);
+    }
+  }
+  return texto;
+}
+
+function btEncriptar() {
+  if (!validar() == true) {
+    const textoEncriptado = encriptar(textoArea.value);
+    mensaje.style.backgroundImage = "none";
+    mensaje.value = textoEncriptado;
+    textoArea.value = "";
+    copia.style.display = "block";
+  }
+}
+
+function btDesencriptar() {
+  if (!validar() == true) {
+    const textoDesencriptado = desencriptar(textoArea.value);
+    mensaje.style.backgroundImage = "none";
+    mensaje.value = textoDesencriptado;
+    textoArea.value = "";
+    copia.style.display = "block";
+  }
+}
+
+function copiar() {
+  mensaje.select();
+  navigator.clipboard.writeText(mensaje.value);
+  mensaje.value = "";
+  alert("Texto copiado");
+
 }
